76fe54a77e15fde15fff35b0ae2818086e4677ef
FM Irshad <irshad@datamation.lk>
2020-02-25 13:37:06 +0530

    Add project files.

---
+++ Pages/Settings/History.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdminLTE.Core.Data;
+using AdminLTE.Core.Services.Mail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminLTE.Core.Pages.Settings
+{
+    public class HistoryModel : PageModel
+    {
+        public HistoryModel() 
+        {
+        }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
